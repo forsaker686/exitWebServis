0a685924fc91130724f5043ff66d89feb4652c43
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2024-12-13 11:02:11 +0100

    dodane datoteke

---
+++ Program.cs
@@ -0,0 +1,24 @@
+/************************************
+ *                v.1               *
+ *      API ZA IZHOD FireFox-a      *
+ *   Izdelava MISIT - Aljaž Krmelj  *
+ ***********************************/
+using System.Diagnostics;
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseWindowsService(); //za potrebe delovanja API-ja kot service - Microsoft Extension hosting windows services
+builder.Services.AddWindowsService(); //za potrebe delovanja API-ja kot service - Microsoft AspNetCore Hosting windowsServices
+builder.Services.AddCors();
+var app = builder.Build(); app.UseHttpsRedirection();
+//app.UseCors(MyAllowSpecificOrigins); // za produkcijo
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod());
+static string UbijProces()
+{
+    //ubijanje procesa firefox-a
+    Process.Start("taskkill", "/F /IM FireFox.exe");
+    return "proces konèan";
+}
+app.MapGet("/exit", () =>
+  UbijProces()
+);
+
+app.Run();
